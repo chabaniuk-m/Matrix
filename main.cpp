4e201f74b4e77a7798a7042898d6bbc9a0798f35
chabanyukm <chabanyuk.maks@gmail.com>
2021-03-21 20:18:33 +0200

    Додана обробка некоректного вводу користувача

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 void showRoots(std::vector<int> roots)
 {
 	std::cout << "Власні числа:\n";
-	int number = 0;
+	int number = 1;
 	int pre_root = INT_MAX;
 	for (auto& k : roots)
 	{
@@ -24,15 +24,29 @@ void showRoots(std::vector<int> roots)
 			pre_root = k;
 		}
 	}
-	std::cout << pre_root;
+	std::cout << pre_root << "\n";
 }
 
 int main()
 {
 	SetConsoleOutputCP(1251);
-	Matrix matrix;
-	matrix.initialize();
-	showRoots(matrix.getEigenValues());
 
-	return 0;
+	Matrix matrix;
+
+	char c;
+	do
+	{
+		matrix.initialize();
+
+		showRoots(matrix.getEigenValues());
+
+		std::cout << "\nПродовжити(1) або вийти(0): ";
+		std::cin >> c;
+		std::cin.ignore();
+		std::cout << "\n";
+	}
+	while (c != '0');
+
+	std::cout << "Програма завершила своє виконання\n";
+	std::cin.get();
 }
